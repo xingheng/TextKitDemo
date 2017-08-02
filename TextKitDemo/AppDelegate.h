d6f22210c1d94dad684e881839702a327925a7d3
Will Han <xingheng907@hotmail.com>
2017-08-02 11:35:54 +0800

    Initial Commit

---
+++ TextKitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TextKitDemo
+//
+//  Created by WeiHan on 02/08/2017.
+//  Copyright © 2017 WillHan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
