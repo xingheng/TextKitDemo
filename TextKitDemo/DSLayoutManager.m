194dfbbee65c9ece44a80059fc1e3a3e64d25576
Will Han <xingheng907@hotmail.com>
2017-08-11 18:00:02 +0800

    Read attribute string from rdfg file for demo.

--- TextKitDemo/DSLayoutManager.m
+++ TextKitDemo/DSLayoutManager.m
@@ -8,6 +8,76 @@
 
 #import "DSLayoutManager.h"
 
+@interface DSLayoutManager () <NSLayoutManagerDelegate>
+
+@end
+
 @implementation DSLayoutManager
+
+- (instancetype)init
+{
+    if (self = [super init]) {
+        self.delegate = self;
+        [self addTextContainer:[self _newTextContainer]];
+    }
+
+    return self;
+}
+
+#pragma mark - Private
+
+- (NSTextContainer *)_newTextContainer
+{
+    NSTextContainer *textContainer = [NSTextContainer new];
+
+    textContainer.size = CGSizeMake(320, 500);
+    return textContainer;
+}
+
+#pragma mark - NSLayoutManagerDelegate
+
+//- (NSUInteger)layoutManager:(NSLayoutManager *)layoutManager shouldGenerateGlyphs:(const CGGlyph *)glyphs properties:(const NSGlyphProperty *)props characterIndexes:(const NSUInteger *)charIndexes font:(UIFont *)aFont forGlyphRange:(NSRange)glyphRange
+//{
+////    CGRect rect = [layoutManager lineFragmentRectForGlyphAtIndex:glyphRange effectiveRange:nil];
+//
+//    NSLog(@"%s", __func__);
+//    return 0;
+//}
+
+- (void)layoutManagerDidInvalidateLayout:(NSLayoutManager *)sender
+{
+    NSLog(@"%s", __func__);
+}
+
+- (void)layoutManager:(NSLayoutManager *)layoutManager didCompleteLayoutForTextContainer:(nullable NSTextContainer *)textContainer atEnd:(BOOL)layoutFinishedFlag
+{
+    if (!textContainer && layoutFinishedFlag) {
+        [self addTextContainer:[self _newTextContainer]];
+        return;
+    }
+
+    NSLog(@"%s", __func__);
+    return;
+    CGRect usedRect = [layoutManager usedRectForTextContainer:textContainer];
+    NSRange range = textContainer ? [layoutManager glyphRangeForTextContainer:textContainer] : NSMakeRange(0, 0);
+
+//    NSLog(@"usedRect: %@, flag: %@, glyphRange: %@, textContainer: %p", NSStringFromCGRect(usedRect), layoutFinishedFlag ? @"YES" : @"NO", NSStringFromRange(range), textContainer);
+    NSLog(@"flag: %@, glyphRange: %@, textContainer: %p", layoutFinishedFlag ? @"YES" : @"NO", NSStringFromRange(range), textContainer);
+
+    for (NSUInteger idx = 0; idx < layoutManager.numberOfGlyphs; idx++) {
+        NSRange outRange;
+        CGRect rect = [layoutManager lineFragmentRectForGlyphAtIndex:idx effectiveRange:&outRange];
+//        NSLog(@"idx: %ld, rect: %@, range: %@, text: %@", idx, NSStringFromCGRect(rect), NSStringFromRange(outRange), [layoutManager.textStorage.string substringWithRange:NSMakeRange(idx, 1)]);
+
+        if (CGRectIsEmpty(rect)) {
+            break;
+        }
+    }
+}
+
+- (void)layoutManager:(NSLayoutManager *)layoutManager textContainer:(NSTextContainer *)textContainer didChangeGeometryFromSize:(CGSize)oldSize
+{
+    NSLog(@"%s", __func__);
+}
 
 @end
--- TextKitDemo/MainViewController.m
+++ TextKitDemo/MainViewController.m
@@ -19,7 +19,7 @@
 
 #define RGB(__r, __g, __b) RGBA(__r, __g, __b, 1.0)
 
-#define RANDOMCOLOR RGB(arc4random_uniform(255), arc4random_uniform(255), arc4random_uniform(255))
+#define RANDOMCOLOR RGB(arc4random_uniform(155), arc4random_uniform(155), arc4random_uniform(155))
 
 
 @interface MainViewController ()
@@ -40,27 +40,55 @@
     DSTextStorage *textStorage = [DSTextStorage new];
     self.textStorage = textStorage;
 
+#if 0
+
     for (NSUInteger idx = 0; idx < 300; idx++) {
         char ca =  (char)(idx % 26) + 97, cb =  (char)(idx % 25) + 98, cc =  (char)(idx % 24) + 99;
 
-        NSAttributedString *attrString = [[NSAttributedString alloc] initWithString:[NSString stringWithFormat:@"[%ld]-[%c%c%c] ", idx, ca, cb, cc]
+        NSAttributedString *attrString = [[NSAttributedString alloc] initWithString:[NSString stringWithFormat:@"[%ld]-[%c%c%c] %@", idx, ca, cb, cc, idx % 20 == 0 ? @"\n\n" : @""]
                                                                          attributes:@{ NSFontAttributeName: [UIFont systemFontOfSize:idx % 20 + 10],
                                                                                        NSForegroundColorAttributeName: RANDOMCOLOR,
                                                                                        NSUnderlineStyleAttributeName: idx % 3 == 0 ? @1 : @0 }];
         [textStorage appendAttributedString:attrString];
     }
 
+#elif 1
+    NSURL *url = [NSBundle.mainBundle URLForResource:@"blog" withExtension:@"rtfd"];
+    NSAttributedString *attrString = [[NSAttributedString alloc] initWithURL:url options:@{ NSDocumentTypeDocumentAttribute: NSRTFDTextDocumentType } documentAttributes:nil error:nil];
+    [textStorage appendAttributedString:attrString];
+    [textStorage appendAttributedString:attrString];
+    [textStorage appendAttributedString:attrString];
+    [textStorage appendAttributedString:attrString];
+#endif
+
     DSLayoutManager *layoutManager = [DSLayoutManager new];
+    layoutManager.allowsNonContiguousLayout = YES;
     [textStorage addLayoutManager:layoutManager];
 
-    DSTextContainer *textContainer = [DSTextContainer new];
+    CGRect frame = CGRectInset(self.view.bounds, 10, 20);
+    DSTextContainer *textContainer = [[DSTextContainer alloc] initWithSize:frame.size];
     [layoutManager addTextContainer:textContainer];
+//
+//    textContainer = [[DSTextContainer alloc] initWithSize:frame.size];
+//    [layoutManager addTextContainer:textContainer];
 
-    UITextView *textview = [[UITextView alloc] initWithFrame:CGRectInset(self.view.bounds, 20, 40)
+//    textContainer = [[DSTextContainer alloc] initWithSize:frame.size];
+//    [layoutManager addTextContainer:textContainer];
+//
+//    textContainer = [[DSTextContainer alloc] initWithSize:frame.size];
+//    [layoutManager addTextContainer:textContainer];
+//
+//    textContainer = [[DSTextContainer alloc] initWithSize:frame.size];
+//    [layoutManager addTextContainer:textContainer];
+
+
+    [layoutManager ensureLayoutForTextContainer:textContainer];
+//    [layoutManager ensureLayoutForBoundingRect:frame inTextContainer:textContainer];
+
+    UITextView *textview = [[UITextView alloc] initWithFrame:frame
                                                textContainer:textContainer];
 
     textview.backgroundColor = [[UIColor lightGrayColor] colorWithAlphaComponent:0.2];
-    textview.frame = CGRectInset(self.view.bounds, 20, 40);
 
     textview.editable = NO;
     textview.allowsEditingTextAttributes = YES;
@@ -75,6 +103,13 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    DSLayoutManager *layoutManager = (DSLayoutManager *)self.textStorage.layoutManagers.firstObject;
+
+//    layoutManager
 }
 
 @end
